4512b18421c1509a59864b92a41e0ba727bc149a
haydende <haydeneastwell@outlook.com>
2023-07-20 22:43:37 +0100

    [kotlin] [WIP] online backup of new repeatedString impl + new test cases

--- kotlin/app/src/main/kotlin/kotlin_solutions/RepeatedString.kt
+++ kotlin/app/src/main/kotlin/kotlin_solutions/RepeatedString.kt
@@ -1,33 +1,15 @@
 package kotlin_solutions
 
+import kotlin.math.roundToLong
+
 /**
  * https://www.hackerrank.com/challenges/repeated-string
  */
 fun repeatedString(s: String, n: Long): Long {
-    var pos = 0
-    var counter = 0L
-    for (i in 0..n ) {
-        val char = s[pos]
-        pos++
-        if ('a'.equals(char)) {
-            counter++
-        }
-        if (pos == s.length) {
-            pos = 0
-        }
-    }
-    return counter
-}
-
-fun repeatedStringAlt(s: String, n: Long): Long {
     val countFromOriginal = s.count { it == 'a' }
-    if (countFromOriginal == s.length) {
-        return n
+    var newCount = 0L
+    if (countFromOriginal != 0) {
+        newCount = (n * (countFromOriginal.toDouble() / s.length.toDouble())).roundToLong()
     }
-    val percentage = countFromOriginal / s.length
-    return n * percentage;
-
-    // deduce percentage of original string containing the character
-    // abc = 1 x a / 3 = 0.33
-    // abcabc = 2 x a / 6
+    return newCount;
 }
--- kotlin/app/src/test/kotlin/kotlin_solutions/RepeatedStringTest.kt
+++ kotlin/app/src/test/kotlin/kotlin_solutions/RepeatedStringTest.kt
@@ -7,14 +7,67 @@ class RepeatedStringTest {
 
     @Test
     fun aba_10_should_be_7() {
-        val actual = repeatedStringAlt("aba", 10L)
+        val actual = repeatedString("aba", 10L)
         assertEquals(7L, actual)
     }
 
     @Test
     fun a_1000000000000_should_be_1000000000000() {
-        val actual = repeatedStringAlt("a", 1000000000000L)
+        val actual = repeatedString("a", 1000000000000L)
         assertEquals(1000000000000L, actual)
+    }
+
+    @Test
+    fun ab_1000000000000_should_be_500000000000() {
+        val actual = repeatedString("ab", 1000000000000L)
+        assertEquals(500000000000L, actual)
+    }
+
+    @Test
+    fun aba_1000000000000_should_be_670000000000() {
+        val actual = repeatedString("aba", 1000000000000L)
+        assertEquals(666666666667L, actual)
+    }
+
+    /**
+     * Input:
+     * - s: gfcaaaecbg
+     * - n: 547602
+     *
+     * Expected Output: 164280
+     */
+    @Test
+    fun testCase4() {
+        val actual = repeatedString("gfcaaaecbg", 547602L)
+        assertEquals(164280L, actual)
+    }
+
+    /**
+     * Input:
+     * - s: epsxyyflvrrrxzvnoenvpegvuonodjoxfwdmcvwctmekpsnamchznsoxaklzjgrqruyzavshfbmuhdwwmpbkwcuomqhiyvuztwvq
+     * - n: 549382313570
+     *
+     * Expected Output: 16481469408
+     */
+    @Test
+    fun testCase9() {
+        val actual = repeatedString("epsxyyflvrrrxzvnoenvpegvuonodjoxfwdmcvwctmekpsnamchznsoxaklzjgrqruyzavshfbmuhdwwmpbkwcuomqhiyvuztwvq", 549382313570L)
+        assertEquals(16481469408, actual)
+    }
+
+    /**
+     * Input:
+     * - s: babbaabbabaababaaabbbbbbbababbbabbbababaabbbbaaaaabbaababaaabaabbabababaabaabbbababaabbabbbababbaabb
+     * - n: 860622337747
+     *
+     * Expected Output: 395886275361
+     */
+    @Test
+    fun testCase11() {
+        val actual = repeatedString(
+            "babbaabbabaababaaabbbbbbbababbbabbbababaabbbbaaaaabbaababaaabaabbabababaabaabbbababaabbabbbababbaabb", 860622337747L
+        )
+        assertEquals(395886275361L, actual)
     }
 
 }
